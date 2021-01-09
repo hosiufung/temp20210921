ee9e0363284ad41709b4060d4099d84fafeb3de3
Triton Ho <5516113+TritonHo@users.noreply.github.com>
2021-01-09 16:27:33 +0000

    add orderBook

--- decode.go
+++ decode.go
@@ -24,24 +24,3 @@ func wssReceiver(c *websocket.Conn, isClosed *atomicBool, wg *sync.WaitGroup) {
 		log.Println("bm, err", bm, err)
 	}
 }
-
-type Order struct {
-	Type       string `json:"TYPE"`
-	M          string `json:"M"`
-	FromSymbol string `json:"FSYM"`
-	ToSymbol   string `json:"TSYM"`
-	//The side is 0 for BID and 1 for ASK
-	Side int `json:"SIDE"`
-	// 1 for ADD (add this position to your ordebook),
-	// 2 for REMOVE (take this position out of your orderbook, REMOVE orders also have a quantity of 0),
-	// 3 for NOACTION (you should not see these messages they represent updates that we receive from the exchange but have no impact on the orderbook) and
-	// 4 for CHANGE/UPDATE (update the available quantity for this position)
-	Action int `json:"ACTION"`
-	CcSeq  int `json:"CCSEQ"`
-	// FIXME: float64 is BAD for price, we need decimal calculation
-	Price      float64 `json:"P"`
-	Amount     float64 `json:"Q"`
-	Seq        int     `json:"SEQ"`
-	ReportedNs int64   `json:"REPORTEDNS"`
-	DelayNs    int64   `json:"DELAYNS"`
-}
